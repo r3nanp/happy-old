094b7f6b074e287afebe5335d58d374d95ab0959
r3nanp <renanmol87@gmail.com>
2020-10-15 13:26:05 -0300

    feat (callout container): create a callout component

---
+++ mobile/src/components/CalloutContainer/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import { Container, MarkerName } from './styles'
+
+const CalloutContainer: React.FC = () => {
+  return (
+    <Container>
+      <MarkerName>Amigos de Jesus</MarkerName>
+    </Container>
+  )
+}
+
+export default CalloutContainer
---
+++ mobile/src/components/CalloutContainer/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  width: 160px;
+  height: 46px;
+  padding-left: 16px;
+  padding-right: 16px;
+  background-color: rgba(255, 255, 255, 0.8);
+  border-radius: 16px;
+  justify-content: center;
+`
+
+export const MarkerName = styled.Text`
+  color: #0089a5;
+  font-size: 14px;
+  font-family: nunito_700;
+`
