53395b25539dbcf185476acf5c93579ad5a8cd8f
r3nanp <renanmol87@gmail.com>
2020-10-16 17:26:58 -0300

    build (images view): change the link to uploads images

--- server/src/views/images_view.ts
+++ server/src/views/images_view.ts
@@ -4,8 +4,7 @@ export default {
   render(image: Image) {
     return {
       id: image.id,
-      url: `http://localhost:3333/uploads/${image.path}`
-
+      url: `http://192.168.0.110:3333/uploads/${image.path}`
     }
   },
 
